eb8f792c87c8b4504170ca894510539f832f5fad
Nitin <nitin8285824551@gmail.com>
2024-03-03 01:26:56 +0530

    connect login user with user state

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
-import React, { useState } from 'react';
-import './App.css';
-import SignUpPage from './pages/SignUpPage';
-import SignInPage from './pages/SignInPage';
-import './FrontPage.css'; // Import CSS for front page buttons
+import React, { useState } from "react";
+import "./App.css";
+import SignUpPage from "./pages/SignUpPage";
+import SignInPage from "./pages/SignInPage";
+import "./FrontPage.css"; // Import CSS for front page buttons
 
 function App() {
-  const [showSignIn, setShowSignIn] = useState(false);
-  const [showSignUp, setShowSignUp] = useState(false);
+	const [user, setUser] = useState(null);
+	const [showSignIn, setShowSignIn] = useState(false);
+	const [showSignUp, setShowSignUp] = useState(false);
 
-  const handleSignInClick = () => {
-    setShowSignIn(true);
-    setShowSignUp(false);
-  };
+	const handleSignInClick = () => {
+		setShowSignIn(true);
+		setShowSignUp(false);
+	};
 
-  const handleSignUpClick = () => {
-    setShowSignIn(false);
-    setShowSignUp(true);
-  };
+	const handleSignUpClick = () => {
+		setShowSignIn(false);
+		setShowSignUp(true);
+	};
 
-  return (
-    <div className="App">
-      <div className="buttons-container">
-        <button onClick={handleSignInClick}>Sign In</button>
-        <button onClick={handleSignUpClick}>Sign Up</button>
-      </div>
-      {showSignIn && <SignInPage />}
-      {showSignUp && <SignUpPage />}
-    </div>
-  );
+  const handleSignOut = () => {
+    setUser(null);
+  }
+
+	return (
+		<div className="App">
+      {user && <h1>Welcome {user.email}, to the Student Portal</h1>}
+			<div className="buttons-container">
+				<button onClick={handleSignInClick}>Sign In</button>
+				<button onClick={handleSignUpClick}>Sign Up</button>
+        {user && <button onClick={handleSignOut}>Sign Out</button>}
+			</div>
+			{showSignIn && <SignInPage  setUser={setUser} />}
+			{showSignUp && <SignUpPage  />}
+		</div>
+	);
 }
 
 export default App;
-
